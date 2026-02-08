87e64d13c2c216bf588aeb111e8d4390d3236286
alrayen <rayen.gaied@esprit.tn>
2026-02-08 17:54:19 +0100

    fixed dynamic size

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
+    /* 
+     * Note: To trigger the "Modifier" overlay, you will need to connect 
+     * a signal from your table widget (e.g., cellDoubleClicked or a specific button in a row)
+     * to a slot that:
+     * 1. Retrieves the data for that row
+     * 2. Populates the "modifier" page fields
+     * 3. Calls ui->overlayModifier->setVisible(true);
+     */
 }
 
 MainWindow::~MainWindow()
